3d6248bc856ccd819ef29f7aefd1bff00425a27f
mdzaid1299@gmail.com <mdzaid1299@gmail.com>
2022-08-25 19:33:05 +0000

    Update GameDemo.java

--- src/main/java/com/jap/GameDemo.java
+++ src/main/java/com/jap/GameDemo.java
@@ -1,27 +1,21 @@
 package com.jap;
 
 public class GameDemo {
-    public String divideByNumber(int firstNumber, int secondNumber){
-        String res = "";
-        try{
-            res = Integer.toString(firstNumber / secondNumber);
-        }catch (ArithmeticException e){
-            res = e.toString();
+
+    public String divideByNumber(int firstNumber, int secondNumber) {
+        //create local variable
+        String temp = "";
+        int finalNumber = 0;
+        try {
+            //write logic
+            finalNumber = firstNumber / secondNumber;
+            temp = Integer.toString(finalNumber);
+
+            //Handle specific exception
+        } catch ( ArithmeticException exception) {
+            temp = exception.toString();
         }
 
-        return res;
-    }
-
-    public static void main(String[] args) {
-        GameDemo obj1 = new GameDemo();
-        GameDemo obj2 = new GameDemo();
-        String output1 = "";
-        String output2 = "";
-        output1 = obj1.divideByNumber(10,5);
-        System.out.println("output = " + output1);
-
-        output2 = obj2.divideByNumber(10,0);
-        System.out.println("output2 = " + output2);
-
+        return temp;
     }
 }
